ec0fc848759cf36ea72088661819cb54ac19fb43
James Nail <james.nail@lanetix.com>
2016-02-16 21:06:41 -0500

    fixed external navigation to app routes

--- src/js/routes/index.jsx
+++ src/js/routes/index.jsx
@@ -9,8 +9,8 @@ import AddMemberContainer from '../containers/add-member'
 export default (
   <Route path='/' component={CoreLayout}>
     <IndexRoute component={HomeView} />
-    <Route path='members/add' component={AddMemberContainer} />
     <Route path='members' component={MembersContainer} />
+    <Route path='members/add' component={AddMemberContainer} />
     <Route path='404' component={NotFoundView} />
     <Redirect from='*' to='404' />
   </Route>
